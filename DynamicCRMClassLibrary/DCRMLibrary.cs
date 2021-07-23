a27774ecec870a20321dcbabb7d4a777ce434480
Thilanka Ranasinghe <thilankaranasinghe1989@gmail.com>
2021-07-23 16:20:26 +1000

    Adding plugin

---
+++ DynamicCRMClassLibrary/DCRMLibrary.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.ServiceModel;
+
+namespace DynamicCRMClassLibrary
+{
+    public class DCRMLibrary : IPlugin
+    {
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Extract the tracing service for use in debugging sandboxed plug-ins.
+            // If you are not registering the plug-in in the sandbox, then you do
+            // not have to add any tracing service related code.
+            ITracingService tracingService =
+                (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.
+                Entity entity = (Entity)context.InputParameters["Target"];
+
+                // Verify that the target entity represents an entity type you are expecting. 
+                // For example, an account. If not, the plug-in was not registered correctly.
+                if (entity.LogicalName != "lead")
+                    return;
+
+                // Obtain the organization service reference which you will need for
+                // web service calls.
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+                    // Plug-in business logic goes here.
+                    Entity followup = new Entity("task");
+
+                    followup["subject"] = "Send  e-mail to the new customer";
+                    followup["description"] = "Followup with the customer";
+                    followup["scheduledstart"] = DateTime.Now.AddDays(7);
+                    followup["scheduledend"] = DateTime.Now.AddDays(7);
+                    followup["category"] = context.PrimaryEntityName;
+
+                    if (context.OutputParameters.Contains("id"))
+                    {
+                        Guid regardingobjectId = new Guid(context.OutputParameters["id"].ToString());
+                        string regardingobjecttype = "lead";
+
+                        followup["regardingobjectid"] = new EntityReference(regardingobjecttype, regardingobjectId);
+                    }
+
+                    service.Create(followup);
+
+                }
+
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in MyPlug-in.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("MyPlugin: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ PluginRegistrationTool/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PluginRegistrationTool
+{
+    public class Class1
+    {
+    }
+}
